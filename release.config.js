0abe718edacb9dbabb4b289aba40f94f0a41fdf1
Sergioandres08 <sergioandrestorrez@gmail.com>
2024-09-22 01:47:16 -0500

    feat: cambio release.config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-    branches: ['main','dev'],
+    branches: ['main'],
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
