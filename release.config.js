a36e22d5868fa9010641b213a504d99d115ffa71
Sergioandres08 <sergioandrestorrez@gmail.com>
2024-09-22 01:55:33 -0500

    feat: cambio release.config

--- release.config.js
+++ release.config.js
@@ -1,12 +1,9 @@
 const config = {
-    branches: ['main'],
+    branches: ['main','dev'],
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
       "@semantic-release/changelog",
-      ["@semantic-release/git", {
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }],
       '@semantic-release/github'
     ]
   };
